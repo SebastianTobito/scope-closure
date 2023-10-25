d7655a376be7514582b1dc2eef5a6079f3d65e21
sebastian tobito saavedra <sebastiantobito@gmail.com>
2023-10-25 17:30:24 -0500

    subiendo scope de bloque

---
+++ src/scope/block.js
@@ -0,0 +1,15 @@
+function frutas() {
+  if (true) {
+    var fruta1 = "manzana"; //function scope
+    let fruta2 = "limon"; //block scope
+    const fruta3 = "banana"; //block scope
+    console.log(fruta2);
+    console.log(fruta3);
+  }
+  console.log(fruta1);
+
+}
+
+frutas();
+
+
