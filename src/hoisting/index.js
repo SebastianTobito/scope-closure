0d09177865907e38241bb366d560ca10fe39e34e
sebastian tobito saavedra <sebastiantobito@gmail.com>
2023-11-11 13:45:20 -0500

    probando el hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,12 @@
+
+console.log(nombreDePerro);
+var nombreDePerro = 'Enzo';
+console.log(nombreDePerro);
+
+
+nombreDelPerro();
+function nombreDelPerro(){
+ console.log(`El mejor perro es ${perro} `);
+}
+
+var perro = 'Enzo';
